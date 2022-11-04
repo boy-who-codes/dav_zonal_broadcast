d350ba792daaeb749aa3a5af684eaaf3ea768d4f
boy-who-codes <rahulsinhaoff@gmail.com>
2022-11-04 18:58:31 +0530

    final

--- src/urls.py
+++ src/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from dist.views import *
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('match/<str:name>', match, name='match'),
     path('cluster/', cluster, name='cluster'),
   
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
