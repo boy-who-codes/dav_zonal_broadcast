f3b8aca8efde08e2b252fe9d23c62f6e8f0e4c78
Rahul Sinha <48615211+boy-who-codes@users.noreply.github.com>
2022-11-04 16:43:37 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_t1o+=4gk5(%v8_lf21g(8=#-%(q_lxa)egs8%la4*scp205o)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bwc-scorecard.herokuapp.com' , 'www.bwc-scorecard.herokuapp.com' , 'https://bwc-scorecard.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
