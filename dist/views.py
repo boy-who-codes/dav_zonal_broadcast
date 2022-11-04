f0472488a71cf5072dc4d7ad72420e969a0613e2
Rishabh Dubey <ravidubey123123123@gmail.com>
2022-11-04 14:40:00 +0530

    filter issue fixed + updated match url

--- dist/views.py
+++ dist/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render ,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.views.generic import ListView, DetailView
 # Create your views here.
+
+
 def home(request):
     sports = Sport.objects.all()
     edit = Edit.objects.all()
@@ -9,21 +11,23 @@ def home(request):
         'sports': sports,
         'edit': edit
     }
-    
+
     return render(request, 'base.html', context)
 
-def match(request , pk):
-    match = get_object_or_404(Match , id=pk)
-    matchs = Match.objects.all()
+
+def match(request, name):
+    sport_id = Sport.objects.filter(name=name).first()
+    matchs = Match.objects.filter(sport=sport_id.id)
     sports = Sport.objects.all()
     context = {
-        'match': matchs ,
-         'sports': sports,
+        'match': matchs,
+        'sports': sports,
         # 'teams': team,
         # 'sport': sport
-        
+
     }
     return render(request, 'match.html', context)
+
 
 def cluster(request):
     cluster = Cluster.objects.all()
--- src/urls.py
+++ src/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from dist.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('match/<int:pk>', match, name='match'),
+    path('match/<str:name>', match, name='match'),
     path('cluster/', cluster, name='cluster'),
-  
-]
 
+]
